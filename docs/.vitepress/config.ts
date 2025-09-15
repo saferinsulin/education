974d522f1f4b208dd8e21780fa48a29e94696251
rhodrijh83 <rhodrijh@doctors.org.uk>
2025-09-15 15:21:05 +0100

    Delete docs directory

--- docs/.vitepress/config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vitepress'
-
-export default defineConfig({
-  base: '/education/', // 👈 matches your repo name exactly
-  title: 'Safer Insulin Education',
-  cleanUrls: true
-})
-
