4901cfea56729c7bf66a3494d4c5a7e399200996
rhodrijh83 <rhodrijh@doctors.org.uk>
2025-09-15 15:07:16 +0100

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,1 +1,8 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  base: '/education/', // 👈 matches your repo name exactly
+  title: 'Safer Insulin Education',
+  cleanUrls: true
+})
 
